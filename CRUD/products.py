1cae633bf84c637b352bb3bc2f083264a84a50a3
fazztx <juan.software@outlook.com>
2025-06-07 18:07:33 -0400

    Created the whole REST API according to the specifications.

--- CRUD/products.py
+++ CRUD/products.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, redirect, request
 import json
 from flask_cors import CORS
 
-app = Flask("Product Server")
+app = Flask("Product Server") #REST APIs for adding, retrieving, updating, and deleting product
 CORS(app)
 
 
@@ -14,5 +14,45 @@ products = [
 #
 #Add all the REST API end-points here
 #
+@app.route('/', methods=['GET'])
+def get_products():
+    return jsonify(products)
 
-app.run(port=5000,debug=True)
+@app.route('/product/<int:id>', methods=['GET'])
+def get_exact_product(id):
+    #id = int(id) 
+    for eachProduct in products:
+        if eachProduct.get("id") == id:
+            return jsonify(eachProduct)
+    return jsonify({"error":'Not found'}), 404
+
+@app.route('/', methods=['POST'])
+def add_product():
+    product =  request.get_json() # Able to receive JSON from body
+    products.append(product)
+    return jsonify({"success":'Added'}), 201 #REST API
+    #return redirect('/') #Route to the first method #WebAPI
+
+@app.route('/product/<int:id>', methods=['PUT'])
+def update_product(id):
+    id = int(id) 
+    for eachProduct in products:
+        if eachProduct.get("id") == id:
+            eachProduct["name"] = request.get_json().get("name")
+            eachProduct["price"] = request.get_json().get("price")
+            return jsonify({"success":'Updated'}), 200
+    return jsonify({"message":'Nothing to update'}), 200
+
+@app.route('/product/<id>', methods=['DELETE'])
+def delete_product(id):
+    id = int(id)
+    product = next((x for x in products if x["id"] == id), None)
+    if product:
+        products.remove(product)
+        return 'Deleted', 204
+    return jsonify({'error': 'Product not found'}), 404 #REST API
+    #return redirect('/') #Route to the first method
+
+
+
+# app.run(port=5000,debug=True)
